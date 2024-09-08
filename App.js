41f7391fd31f7d4b377a17a3ad889d4fe44de0f6
iamsubhankarray <iamsubhankarray@gmail.com>
2024-09-09 04:59:31 +0530

    app updated

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      {/* <Ionicons name='checkmark'size={32} color="green"/> */}
     </View>
   );
 }
@@ -24,11 +26,11 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="home" component={HomeScreen} />
-        <Tab.Screen name="reOrder" component={HomeScreen} />
-        <Tab.Screen name="cart" component={HomeScreen} />
-        <Tab.Screen name="account" component={SettingsScreen} />
+      <Tab.Navigator screenOptions={{}}>
+        <Tab.Screen name="home" component={HomeScreen} options={{tabBarIcon:()=><AntDesign name="home" size={24} color="black" />}} />
+        <Tab.Screen name="reOrder" component={HomeScreen} options={{tabBarIcon:()=><AntDesign name="user" size={24} color="black" />}} />
+        <Tab.Screen name="cart" component={HomeScreen} options={{tabBarIcon:()=> <AntDesign name="shoppingcart" size={24} color="black" />}}/>
+        <Tab.Screen name="account" component={SettingsScreen}options={{tabBarIcon:()=><AntDesign name="user" size={24} color="black" />}} />
       </Tab.Navigator>
     </NavigationContainer>
   );
