c74768651741bb9412c908a1888cb77d5f057a41
iamsubhankarray <iamsubhankarray@gmail.com>
2024-09-10 23:42:49 +0530

    product card updated

--- component/Products.js
+++ component/Products.js
@@ -1,20 +1,27 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Products = () => {
+  const [like,setlike]=useState(false)
   return (
-    <View style={{ flex: 1,justifyContent:"center",alignItems:"center" }}>
-      <View>
+    <TouchableOpacity style={{ flex: 1,justifyContent:"center",alignItems:"center" }} onPress={()=>setlike(!like)}>
+      <View >
 
-      <AntDesign name="heart" size={30} color="orange"/>
+
       <Image
         source={require('../assets/girl.jpg')}
-        style={{ width: 180, height: 256, borderRadius: 20, }} />
+        style={{ width: 180, height: 256, borderRadius: 20, }}
+         />
         </View>
       <Text style={{ fontSize: 20, color: "orange", }}>ProductName</Text>
       <Text style={{ fontSize: 15, color: "red", }}>RS 2,000/-</Text>
-    </View>
+        <View style={{position:"absolute",top:9,right:20,height:25,width:25,borderRadius:20,justifyContent:"center",alignContent:"center"}}>
+         { like?
+      <AntDesign name="heart" size={25} color="orange"/>:
+      <AntDesign name="heart" size={25} color="white"/>}
+        </View>
+    </TouchableOpacity>
   )
 }
 
